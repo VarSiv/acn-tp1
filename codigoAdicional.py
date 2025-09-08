2e0aaf328b777466349e1833e48d07ca1d973162
Catalina Chab López <cchablopez@mail.utdt.edu.com>
2025-09-07 21:08:50 -0300

    arhivo adicional

---
+++ codigoAdicional.py
@@ -0,0 +1,33 @@
+'''
+class Avion:
+    _NEXT_ID = 0
+    def __init__(self, velocidad_kmh, distancia_km):
+        self.id = Avion._NEXT_ID; Avion._NEXT_ID += 1
+        self.velocidad = velocidad_kmh   # km/h
+        self.distancia = distancia_km    # km
+        self.sentido = +1                # +1: hacia la pista, -1: retroceso
+        self.en_retroceso = False
+
+    def iniciar_retroceso(self):
+        self.sentido = -1
+        self.velocidad = 200 * 1.852     # 200 kt
+        self.en_retroceso = True
+
+    def terminar_retroceso(self):
+        self.sentido = +1
+        self.en_retroceso = False
+
+    def actualizar(self, delta_horas):
+        avance = self.velocidad * delta_horas * self.sentido
+        # si sentido = +1: distancia -= (v*dt); si -1: distancia aumenta
+        self.distancia = max(0.0, self.distancia - avance)
+
+        # (solo si se acerca) setear la Vmax por rango
+        if self.sentido == +1:
+            d = self.distancia
+            if d > 100*1.852:      self.velocidad = 500*1.852
+            elif d > 50*1.852:     self.velocidad = 300*1.852
+            elif d > 15*1.852:     self.velocidad = 250*1.852
+            elif d > 5*1.852:      self.velocidad = 200*1.852
+            else:                  self.velocidad = 150*1.852
+'''
